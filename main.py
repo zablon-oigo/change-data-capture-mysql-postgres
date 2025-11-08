189a3196b1c9f863b49adeef9e048b273f295681
zablon <zablonoigo51@gmail.com>
2025-11-08 09:44:23 +0300

    simple webserver

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from books!")
+from fastapi import FastAPI, Header, status
+from  typing import Optional, List
+from fastapi.exceptions import HTTPException
+from pydantic import BaseModel
 
+app=FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def index():
+    return{"Hello World"}
+
+@app.get('/get_headers')
+async def get_all_request_headers(
+    user_agent: Optional[str] = Header(None),
+    accept_encoding: Optional[str] = Header(None),
+    referer: Optional[str] = Header(None),
+    connection: Optional[str] = Header(None),
+    accept_language: Optional[str] = Header(None),
+    host: Optional[str] = Header(None),
+):
+    request_headers = {}
+    request_headers["User-Agent"] = user_agent
+    request_headers["Accept-Encoding"] = accept_encoding
+    request_headers["Referer"] = referer
+    request_headers["Accept-Language"] = accept_language
+    request_headers["Connection"] = connection
+    request_headers["Host"] = host
+
+    return request_headers
+
