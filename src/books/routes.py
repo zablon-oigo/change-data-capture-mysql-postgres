42ab63b4d5fbced26314f3eb182f02342ad9dcf6
zablon <zablonoigo51@gmail.com>
2025-11-23 21:19:14 +0300

    add create method

--- src/books/routes.py
+++ src/books/routes.py
@@ -28,3 +28,26 @@ async def get_all_books(
 ):
     books = await book_service.get_all_books(session)
     return books
+
+
+
+@book_router.get("/{book_uid}", response_model=BookReadModel, status_code=status.HTTP_200_OK)
+async def get_book(
+    book_uid: str, 
+    session: AsyncSession = Depends(get_session),
+    _: bool = Depends(user_or_admin_checker)
+):
+    book = await book_service.get_book(book_uid, session)
+    if not book:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+    return book
+
+
+@book_router.post("/", response_model=BookReadModel, status_code=status.HTTP_201_CREATED)
+async def create_book(
+    book_data: BookCreateModel, 
+    session: AsyncSession = Depends(get_session),
+    _: bool = Depends(admin_checker)  
+):
+    new_book = await book_service.create_book(book_data, session)
+    return new_book
