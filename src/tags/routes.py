304ea0c8528aa0008c8cb1a957a31fdfa51943bb
zablon <zablonoigo51@gmail.com>
2025-12-14 11:58:21 +0300

    add roles

--- src/tags/routes.py
+++ src/tags/routes.py
@@ -9,3 +9,12 @@ from src.books.schemas import BookReadModel
 
 from .schemas import TagAddModel, TagCreateModel, TagModel
 from .services import TagService
+
+tags_router = APIRouter()
+tag_service = TagService()
+user_role_checker = Depends(RoleChecker(["user", "admin"]))
+
+
+@tags_router.get("/", response_model=List[TagModel], dependencies=[user_role_checker])
+async def get_all_tags(session: AsyncSession = Depends(get_session)):
+    return await tag_service.get_tags(session)
