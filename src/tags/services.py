e4aa4957c9848cf0313a948c6cfd7b790ce0426a
zablon <zablonoigo51@gmail.com>
2025-12-16 09:41:01 +0300

    add tags services

--- src/tags/services.py
+++ src/tags/services.py
@@ -11,3 +11,9 @@ book_service = BookService()
 
 
 class TagService:
+    
+    async def get_tags(self, session: AsyncSession):
+        """Get all tags"""
+        statement = select(Tag).order_by(desc(Tag.created_at))
+        result = await session.exec(statement)
+        return result.all()
