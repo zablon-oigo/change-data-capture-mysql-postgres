b6dbf803dc4c4bcbbab23010b2543c1c4f947bc3
zablon <zablonoigo51@gmail.com>
2025-12-20 21:51:15 +0300

    create a new user

--- src/auth/services.py
+++ src/auth/services.py
@@ -22,3 +22,18 @@ class UserService:
         result = await session.execute(select(User))
         users = result.scalars().all()
         return users
+    
+
+    async def create_user(self, user_data: UserCreateModel, session: AsyncSession) -> User:
+        """Create a new user account."""
+        user_dict = user_data.model_dump()
+        new_user = User(
+            uid=str(uuid.uuid4()),                
+            username=user_dict["username"],
+            first_name=user_dict.get("first_name"),
+            last_name=user_dict.get("last_name"),
+            email=user_dict["email"],
+            password_hash=generate_password_hash(user_dict["password"]),
+            is_verified=user_dict.get("is_verified", False),
+            created_at=datetime.utcnow(),
+        )
