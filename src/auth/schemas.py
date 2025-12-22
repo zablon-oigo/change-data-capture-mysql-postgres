92e2502bc99d96f4c5023cce1947c3252ca72b0a
zablon <zablonoigo51@gmail.com>
2025-12-22 22:11:03 +0300

    add usermodel

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -27,3 +27,19 @@ class UserCreateModel(BaseModel):
 class UserLoginModel(BaseModel):
     email: EmailStr
     password: str
+
+
+class UserModel(BaseModel):
+    uid: uuid.UUID
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool
+    password_hash: str = Field(exclude=True)
+    created_at: datetime
+    updated_at: datetime  
+
+    model_config={
+        "form_attributes": True
+    }
