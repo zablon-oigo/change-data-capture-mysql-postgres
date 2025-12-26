33945344e418dc5acb4bfe9fa77893c50bc1eea9
zablon <zablonoigo51@gmail.com>
2025-12-27 00:00:38 +0300

    password reset confirm

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -223,3 +223,18 @@ async def password_reset_request(email_data: PasswordResetRequestModel):
         },
         status_code=status.HTTP_200_OK,
     )
+
+
+@auth_router.post("/password-reset-confirm/{token}")
+async def reset_account_password(
+    token: str,
+    passwords: PasswordResetConfirmModel,
+    session: AsyncSession = Depends(get_session),
+):
+    new_password = passwords.new_password
+    confirm_password = passwords.confirm_new_password
+
+    if new_password != confirm_password:
+        raise HTTPException(
+            detail="Passwords do not match", status_code=status.HTTP_400_BAD_REQUEST
+        )
