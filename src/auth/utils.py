b14b6d79bf4c7a9d0a7feaedc4993327cb6a10ef
zablon <zablonoigo51@gmail.com>
2025-12-28 00:39:48 +0300

    add email config

---
+++ src/auth/utils.py
@@ -0,0 +1,15 @@
+import uuid
+import logging
+from datetime import datetime, timedelta
+from itsdangerous import URLSafeTimedSerializer
+from itsdangerous import URLSafeTimedSerializer, BadSignature, SignatureExpired
+from fastapi import HTTPException
+
+import jwt
+from passlib.context import CryptContext
+from src.config import Config
+
+serializer = URLSafeTimedSerializer(
+    secret_key=Config.JWT_SECRET, 
+    salt="email-configuration"
+)
