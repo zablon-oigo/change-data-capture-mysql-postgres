9edf2e95a036ce9077d48ebeb7a980204273990f
zablon <zablonoigo51@gmail.com>
2025-12-28 00:41:05 +0300

    verify passwd

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -18,3 +18,7 @@ passwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def generate_password_hash(password: str) -> str:
     return passwd_context.hash(password)
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    return passwd_context.verify(password, hashed_password)
+
